a7cc5d0714d34ea5f9279c22a75025d47c8bede3
Rumi Nakamura <krw.psh2196@gmail.com>
2020-06-21 02:01:23 +0000

    user_paramsを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,12 @@ class UsersController < ApplicationController
   end
   
   def create
+    @user = User.new(user_params)
+    if @user.save
+      
+    else
+      render :new
+    end
   end
   
   def edit
@@ -22,4 +28,9 @@ class UsersController < ApplicationController
   
   def destroy
   end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
