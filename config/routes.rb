498be8389dd0eefaa09c9b117303c4ceb7e356d7
Rumi Nakamura <krw.psh2196@gmail.com>
2020-06-21 21:09:02 +0000

    ログインページを編集中

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages#top'
-  get '/signup', to: 'users#new'
+  root    'static_pages#top'
+  get     '/signup', to: 'users#new'
+  
+  #ログイン機能
+  get     '/login', to: 'sessions#new'
+  post    '/login', to: 'sessions#create'
+  delete  '/login', to: 'sessions#destroy'
+  
   resources :users
 end
