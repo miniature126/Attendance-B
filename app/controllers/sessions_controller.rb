f254b8b8c42bdd59e0ca816a2b30df7a292f6597
Rumi Nakamura <krw.psh2196@gmail.com>
2020-06-22 13:07:24 +0000

    ログイン機能を追加中

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 class SessionsController < ApplicationController
   def new
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authentiate(params[:session][:passwor])
+    else 
+      render :new
+    end
   end
-
+  
+  def create
+    render :new
+  end
 end
