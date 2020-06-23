ecbe219f8e153ba9149d7f9d76217200390a249e
Rumi Nakamura <krw.psh2196@gmail.com>
2020-06-23 14:22:08 +0000

    current_user機能を追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,14 @@
 module SessionsHelper
   
-  #引数に渡されたユーザーオブジェクトでログイン
+  #ユーザーIDを一時的セッションの中に記憶
   def log_in(user)
     session[:user_id] = user.id
+  end
+  
+  #現在ログイン中のユーザーがいる場合、オブジェクトを返す
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: params[:user_id])
+    end
   end
 end
