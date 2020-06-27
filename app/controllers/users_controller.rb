5757a0595fecd8f32367d1d41e6090220c5d0d59
Rumi Nakamura <krw.psh2196@gmail.com>
2020-06-27 09:28:00 +0000

    フレンドリーフォワーディング機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,7 @@ class UsersController < ApplicationController
     #ログイン済みユーザーか確認
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "ログインしてください。"
         redirect_to login_url
       end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -43,12 +43,23 @@ module SessionsHelper
   end
   
   #渡されたユーザーがログイン済みのユーザーであればtrueを返す
-  def current_user?
+  def current_user?(user)
     @user == current_user
   end
   
   #現在ログイン中のユーザーがいる場合はtrue、居ない場合はfalseを返す
   def logged_in?
     !current_user.nil?
+  end
+  
+  #記憶しているURL(またはデフォルトURL)にリダイレクトする
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+  
+  #アクセスしようとしたURLを記憶する
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
