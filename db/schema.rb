5566c3b56f4454591768983d6774b2c3eaeb6e53
Rumi Nakamura <krw.psh2196@gmail.com>
2020-06-30 10:52:19 +0000

    時間が表示されるよう詳細ページのビューを修正

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20200630103551) do
     t.boolean "admin", default: false
     t.string "department"
     t.datetime "basic_time", default: "2020-06-29 23:00:00"
-    t.datetime "work_time"
+    t.datetime "work_time", default: "2020-06-29 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
