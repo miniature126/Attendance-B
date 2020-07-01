2b21d062e6176df278048e8f766ff9668153ff4e
Rumi Nakamura <krw.psh2196@gmail.com>
2020-07-01 10:16:07 +0000

    勤怠基本情報更新機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,16 +46,24 @@ class UsersController < ApplicationController
   end
 
   def edit_basic_info
-    
   end
   
   def update_basic_info
-    
+    if @user.update_attributes(basic_info_params)
+      flash[:success] = "基本情報を更新しました。"
+    else
+      flash[:danger] = "#{@user.name}の基本情報更新に失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
+      redirect_to users_url
   end
   
   private
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+    end
+    
+    def basic_info_params
+      params.require(:user).permit(:department, :basic_time, :work_time)
     end
     
     #paramsハッシュからユーザー情報を取得
