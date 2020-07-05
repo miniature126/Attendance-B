416aaee3d505e22d8b8edb9bdf6e55f9dac67902
Rumi Nakamura <krw.psh2196@gmail.com>
2020-07-05 13:11:21 +0000

    勤怠情報更新フォームの入力制限を追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -32,11 +32,11 @@ class AttendancesController < ApplicationController
     ActiveRecord::Base.transaction do #トランザクションを開始
       attendances_params.each do |id, item|
         attendance = Attendance.find(id)
-        attendance.uptate_attributes!(item)
+        attendance.update_attributes!(item)
       end
     end
     flash[:success] = "１ヶ月分の勤怠情報を更新しました。"
-    redirect_to1 user_url(date: params[:date])
+    redirect_to user_url(date: params[:date])
   rescue ActiveRecord::RecordInvalid #トランザクションによるエラー分岐
     flash[:danger] = "無効なデータ入力があった為、更新をキャンセルしました。"
     redirect_to attendances_edit_one_month_user_url(date: params[:date])
