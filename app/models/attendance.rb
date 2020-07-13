5af7fb53dc754d9c1cd6a7e6d748c84f0c4726e7
Rumi Nakamura <krw.psh2196@gmail.com>
2020-07-13 12:17:40 +0000

    検索フォームの表示を追加中

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -6,7 +6,7 @@ class Attendance < ApplicationRecord
   
   #出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
-  #出勤・退勤時間がどちらも存在する時、出金時間より早い退勤時間は無効
+  #出勤・退勤時間がどちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_then_finished_at_fast_if_invalid
   #当日の勤怠データが存在する時、出勤時間のみの更新は無効
   validate :started_at_is_invalid_without_a_finished_at
