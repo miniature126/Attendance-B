326ec4cae2230ae6eb234ed41bbf306d3ade1e47
Rumi Nakamura <krw.psh2196@gmail.com>
2020-07-18 08:20:02 +0000

    出勤・退勤時間の値を受け取って15分単位に直すヘルパーメソッドを追加中

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -10,6 +10,20 @@ module AttendancesHelper
     false
   end
   
+  #出勤時間を受け取り、15分単位にしたものを返す
+  def started_at_time
+    str = @attendance.started_at
+    if str.min.between?(0, 14)
+      @str_display = str.change(min: 0)
+    elsif str.min.between?(15, 29)
+      @str_display = str.change(min: 15)
+    elsif str.min.between?(30, 44)
+      @str_display = str.change(min: 30)
+    elsif str.min.between?(45, 59)
+      @str_display = str.change(min: 45)
+    end
+  end
+  
   #出勤時間と退勤時間を受け取り、在社時間を計算して返す
   def working_times(start, finish)
     format("%.2f", (((finish - start) / 60) / 60.0))
