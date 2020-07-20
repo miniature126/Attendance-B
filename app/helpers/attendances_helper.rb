927fdb7850d172be2344d0dfcdaa1718f36226be
Rumi Nakamura <krw.psh2196@gmail.com>
2020-07-20 06:38:42 +0000

    勤怠B完成

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -10,22 +10,6 @@ module AttendancesHelper
     false
   end
   
-  #出勤時間を受け取り、15分単位にしたものを返す
-  # def time_convertion(str_fin)
-  #   @display_time = str_fin
-  #   case @display_time.min
-  #   when 0..14
-  #     @display_time.change(min: 0)
-  #   when 15..29
-  #     @display_time.change(min: 15)
-  #   when 30..44
-  #     @display_time.change(min: 30)
-  #   when 45..59
-  #     @display_time.change(min: 45)
-  #   else 
-  #   end
-  # end
-  
   #出勤時間と退勤時間を受け取り、在社時間を計算して返す
   def working_times(start, finish)
     format("%.2f", (((finish.floor_to(15.minute) - start.floor_to(15.minute)) / 60) / 60.0))
